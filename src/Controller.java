b8fe6c21abc4af3feea0306b5c2e7cc7435b7862
MarekReimal <marekreimal@gmail.com>
2023-05-22 19:41:39 +0300

    Ümardamine muudetud, loop eemaldatud

--- src/Controller.java
+++ src/Controller.java
@@ -8,13 +8,16 @@ public class Controller {
     public void run(){
         this.view.showInfo(); // kuvab info mida programm teeb
         while (!this.s) { // sisestab andmeid kuni s true
+            // on küll loop, aga küsib ühe korra, all pool lõpetab kohe
             String sideA = this.view.ask(); // küsib külje pikkuse
             try {
                 double sideIntA = Double.parseDouble(sideA); // teisendab str>int
                 ShapeData side = new ShapeData(sideIntA); // loob andmeobjekti ja kirjutab listi
             } catch (NumberFormatException e) {
                 this.s = true; // sisestati s ja lõpetab küsimise
+                System.out.println("Sisesta täisarv või murdarv .-ga, prog. lõpetab");
             }
+        this.s = true; // küsib ainult ühe korra andmeid ja lõpetab loopi
         }
         this.view.showData(); //
     }
--- src/View.java
+++ src/View.java
@@ -4,13 +4,13 @@ public class View {
     private static final Scanner scanner= new Scanner(System.in); // lugemise jaoks input
 
     public void showInfo(){
-        System.out.println("Saab arvutada ühe või mitme ristküliku andmed");
+        //System.out.println("Saab arvutada ühe või mitme ristküliku andmed");
         System.out.println("Kasutaja sisestab ühe külje, teine külg on juhuslik vahemikus " +
                 ShapeData.MINIMUM + " - " + ShapeData.MAXIMUM );
 
     }
     public String ask(){
-        System.out.println("Sisesta külg A, lõpetamiseks vajuta s");
+        System.out.println("Sisesta külg A"); //, lõpetamiseks vajuta s");
         String sideA = scanner.nextLine();
         return sideA;
 
@@ -19,8 +19,11 @@ public class View {
         int i = 0; // järjekorra number
         for (ShapeData p : ShapeData.shapeDataList) {
             i += 1; // jnr kasvab
-            System.out.printf("%s. ristküliku külg a = %.1f; külg b = %.1f; P = %.1f; S = %.1f; D = %.1f\n",
-                    i, p.sideA, p.sideB, p.perm, p.area, p.diagonal); // %d int vorm, %.1f murd 1 koht peale koma
+                // esimene variant printf formindab 1 koht peale koma
+//            System.out.printf("%s. ristküliku külg a = %.1f; külg b = %.1f; P = %.1f; S = %.1f; D = %.1f\n",
+//            i, p.sideA, p.sideB, p.perm, p.area, p.diagonal); // %d int vorm, %.1f murd 1 koht peale koma
+            System.out.printf("Ristküliku parameetrid on järgmised:%nkülg a = %f; külg b = %f; P = %f; S = %.1f; D = %f\n",
+                    p.sideA, p.sideB, p.perm, p.area, p.diagonal); // %d int vorm, %.1f murd 1 koht peale koma
         }
     }
 }
